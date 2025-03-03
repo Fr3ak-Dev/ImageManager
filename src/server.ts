f8eb1807cfd865d8369f1bdb00b8ba8fc7ce717e
gustavo <kg.2k.pl@gmail.com>
2025-03-03 00:05:41 -0400

    Refactor server structure by introducing a router module for API endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express'; // ESM Esmascript Module import
+import router from './router';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Image Manager');
-});
+app.use('/api', router);
 
 export default app;
