c5679306626292add92afcecc2a76b849f580ad2
gustavo <kg.2k.pl@gmail.com>
2025-03-03 03:03:29 -0400

    Add colors package for terminal

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from 'colors';
 import server from './server';
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(colors.cyan.bold(`Server listening at http://localhost:${port}`));
 });
